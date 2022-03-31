b13c98cbed5d677f0924f5b72a5e9130971259ff
Pukar <68369744+Pukarojha@users.noreply.github.com>
2022-03-31 09:00:46 +0545

    user login

--- server/Routes/Home.js
+++ server/Routes/Home.js
@@ -3,14 +3,51 @@ const router = express.Router();
 const db = require("../db");
 const app = express();
 
-router.post("/welcome", (req, res) => {
-  const { password, confirm } = req.body;
+const multer = require("multer");
 
-  // console.log(path);
-  console.log(password, confirm);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/post/img/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}${getExt(file.mimetype)}`);
+  },
+});
+
+const getExt = (mimetype) => {
+  switch (mimetype) {
+    case "image/png":
+      return ".png";
+    case "image/jpeg":
+      return ".jpeg";
+    case "image/jpg":
+      return ".jpg";
+  }
+};
+
+const filefilter = (req, file, cb) => {
+  if (
+    file.mimetype == "image/jpeg" ||
+    file.mimetype == "image/jpg" ||
+    file.mimetype == "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: filefilter });
+
+router.post("/post", upload.single("image"), (req, res) => {
+  const { text, photo, video } = req.body;
+
+  let path = req.file.destination + req.file.filename;
+  console.log(path);
+  console.log(text, photo, video);
   db.query(
-    "INSERT INTO person(password, confirm) VALUES(password, confirm)",
-    [password, confirm],
+    "INSERT INTO content(text,photo, video) VALUES(?, ?, ?)",
+    [text, photo, video],
     (err, result) => {
       if (err) {
         console.log(err);
